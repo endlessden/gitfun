593e66879bfa7ec6a2b91d56b3e22728f20415e1
endlessden <mouldxie@163.com>
2018-09-11 19:12:09 +0800

    finally chapter

--- gittest.py
+++ gittest.py
@@ -10,3 +10,10 @@ print('the files changes')
 print('maybe merge conflict')
 
 print('try to modify the file and commit again')
+
+"""
+clone , add, commit,
+push , pull, reset
+this is how we use git.
+
+"""
